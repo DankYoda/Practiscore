ca5dbb783ba53a125b8cead136333d8622d853b4
dankyoda <dakotafeist@protonmail.com>
2025-06-18 19:16:48 -0500

    Restructure project

--- server/config/bundles.php
+++ server/config/bundles.php
@@ -9,4 +9,6 @@ return [
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     ApiPlatform\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
+    Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
+    Symfony\UX\StimulusBundle\StimulusBundle::class => ['all' => true],
 ];
