4ffb98091ec46e9956c32fac12493c8cac8e3f4c
DankYoda <167357217+DankYoda@users.noreply.github.com>
2025-07-02 09:12:36 -0500

    Create UserRepository.php

---
+++ server/src/Repository/UserRepository.php
@@ -0,0 +1,36 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Repository;
+
+use App\Model\Entity\User;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Bridge\Doctrine\Security\User\UserLoaderInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+class UserRepository extends ServiceEntityRepository implements UserLoaderInterface {
+	public function __construct(ManagerRegistry $registry) {
+		parent::__construct($registry, User::class);
+	}
+
+	public function save(User $entity): void {
+		$this->getEntityManager()->persist($entity);
+	}
+
+	public function remove(User $entity, bool $flush = false): void {
+		$this->getEntityManager()->remove($entity);
+	}
+
+	public function loadUserByIdentifier(string $identifier): ?UserInterface {
+		$entityManager = $this->getEntityManager();
+
+		return $entityManager->createQuery(
+			'SELECT u
+                FROM App\Model\Entity\User u
+                WHERE u.id = :query'
+		)
+			->setParameter('query', $identifier)
+			->getOneOrNullResult();
+	}
+}
