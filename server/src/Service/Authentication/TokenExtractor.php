44b3d13ced4ecfedac9354b094f51ba7db1d8ea8
DankYoda <167357217+DankYoda@users.noreply.github.com>
2025-07-07 09:14:53 -0500

    Create TokenExtractor.php

---
+++ server/src/Service/Authentication/TokenExtractor.php
@@ -0,0 +1,23 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Service\Authentication;
+
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Http\AccessToken\AccessTokenExtractorInterface;
+use Symfony\Component\Security\Core\Exception\CustomUserMessageAuthenticationException;
+
+class TokenExtractor implements AccessTokenExtractorInterface
+{
+	function __construct(
+		private readonly string $appIdentifier,
+	)
+	{
+		
+	}
+	public function extractAccessToken(Request $request): ?string
+	{
+		return $request->cookies->get($this->appIdentifier);
+	}
+}
