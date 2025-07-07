c5bc98cd197d5cbeaaa30a6374a7faa429da98c8
DankYoda <167357217+DankYoda@users.noreply.github.com>
2025-07-07 09:20:13 -0500

    Create SecurityController.php

---
+++ server/src/Controller/SecurityController.php
@@ -0,0 +1,25 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use App\Service\Cookie\CookieToken;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Attribute\AsController;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[AsController]
+class SecurityController
+{
+	function __construct(
+		private readonly CookieToken $cookieToken
+	)
+	{
+	}
+	
+	#[Route('/logout', name: 'app_logout', methods: ['POST'])]
+	public function logout(): Response
+	{
+		return $this->cookieToken->cookieTokenDelete();
+	}
+}
