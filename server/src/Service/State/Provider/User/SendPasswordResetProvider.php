64be692581852bb9da332d00d01e95d607311002
DankYoda <167357217+DankYoda@users.noreply.github.com>
2025-07-07 14:14:15 -0500

    Update SendPasswordResetProvider.php

--- server/src/Service/State/Provider/User/SendPasswordResetProvider.php
+++ server/src/Service/State/Provider/User/SendPasswordResetProvider.php
@@ -1,8 +1,18 @@
 <?php
+declare(strict_types=1);
 
 namespace App\Service\State\Provider\User;
 
-class SendPasswordResetProvider
-{
+use ApiPlatform\Metadata\Operation;
+use ApiPlatform\State\ProviderInterface;
+use App\Repository\UserRepository;
 
+readonly class SendPasswordResetProvider implements ProviderInterface {
+
+    public function __construct(private UserRepository $userRepository) {
+    }
+
+    public function provide(Operation $operation, array $uriVariables = [], array $context = []): object|array|null {
+        return $this->userRepository->findOneBy(['email' => $uriVariables['email']]);
+    }
 }
