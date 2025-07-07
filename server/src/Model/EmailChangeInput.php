4332d210df61a6373f89f65c267d1a75f9d218c3
DankYoda <167357217+DankYoda@users.noreply.github.com>
2025-07-07 14:22:57 -0500

    Create EmailChangeInput.php

---
+++ server/src/Model/EmailChangeInput.php
@@ -0,0 +1,12 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Model;
+
+use App\Service\Validator\Constraint\ApprovedEmail;
+use Symfony\Component\Validator\Constraints as Assert;
+
+class EmailChangeInput {
+    #[Assert\NotBlank]
+    public string $email;
+}
