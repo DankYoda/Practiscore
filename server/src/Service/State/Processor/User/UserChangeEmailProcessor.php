925448295da3acd69b9f9f25c369a1c389733146
DankYoda <167357217+DankYoda@users.noreply.github.com>
2025-07-07 14:23:40 -0500

    Create UserChangeEmailProcessor.php

---
+++ server/src/Service/State/Processor/User/UserChangeEmailProcessor.php
@@ -0,0 +1,38 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Service\State\Processor\User;
+
+use ApiPlatform\Metadata\Operation;
+use ApiPlatform\State\ProcessorInterface;
+use ApiPlatform\Symfony\Security\Exception\AccessDeniedException;
+use App\Repository\UserRepository;
+use App\Service\EmailVerifier;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpKernel\Exception\UnprocessableEntityHttpException;
+
+class UserChangeEmailProcessor implements ProcessorInterface
+{
+	function __construct(
+		private readonly EmailVerifier $emailVerifier,
+		private readonly EntityManagerInterface $entityManager,
+        private readonly UserRepository $userRepository
+	)
+	{
+	}
+
+	public function process(mixed $data, Operation $operation, array $uriVariables = [], array $context = [])
+	{
+        $user = $this->userRepository->find($uriVariables['userId']);
+		if (!$user->getPassword())
+			throw new AccessDeniedException();
+		if ($this->userRepository->findBy(['email'=>$data->email]))
+			throw new UnprocessableEntityHttpException('That email is already used');
+		$user->setEmailVerified(false);
+        $user->setEmail($data->email);
+		$this->entityManager->persist($user);
+		$this->entityManager->flush();
+		$this->emailVerifier->sendEmail($user);
+		return $user;
+	}
+}
