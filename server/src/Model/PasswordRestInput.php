b2151b993c1c8bb5f20804e78d5424753f4a52b4
DankYoda <167357217+DankYoda@users.noreply.github.com>
2025-07-07 14:04:56 -0500

    Create PasswordRestInput.php

---
+++ server/src/Model/PasswordRestInput.php
@@ -0,0 +1,16 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Model;
+
+use Symfony\Component\Validator\Constraints as Assert;
+
+class PasswordResetInput
+{
+	#[Assert\NotBlank]
+	#[Assert\Length(
+		min: 14,
+		minMessage: 'Your password must be at least 14 characters long'
+	)]
+	public string $newPassword;
+}
