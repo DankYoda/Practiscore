fcad90a372ad8ba260e7710d214cf75516505e93
DankYoda <167357217+DankYoda@users.noreply.github.com>
2025-07-08 09:50:17 -0500

    Update VerificationUrlGenerator.php

--- server/src/Service/UrlGenerator/VerificationUrlGenerator.php
+++ server/src/Service/UrlGenerator/VerificationUrlGenerator.php
@@ -1,8 +1,22 @@
 <?php
+declare(strict_types=1);
 
 namespace App\Service\UrlGenerator;
 
-class VerificationUrlGenerator
-{
+use League\Uri\UriTemplate;
 
+readonly class VerificationUrlGenerator {
+    function __construct(private ClientUrlGenerator $urlGenerator) {}
+
+    /**
+     * Generate a UriTemplate based on the context of the request.
+     *
+     * `$this->generator->generate()->expand(['userId' => $userId, 'token' => $token])`
+     *
+     * @return UriTemplate
+     * @see https://uri.thephpleague.com/uri/7.0/uri-template/
+     */
+    public function generate(): UriTemplate {
+        return new UriTemplate("{$this->urlGenerator->getClientUrl()}#/verify?userId={userId}&token={token}");
+    }
 }
