f8c1e5caa61faa6b036c3ffb0c25cbed99379df2
DankYoda <167357217+DankYoda@users.noreply.github.com>
2025-07-08 09:50:05 -0500

    Update ResetUrlGenerator.php

--- server/src/Service/UrlGenerator/ResetUrlGenerator.php
+++ server/src/Service/UrlGenerator/ResetUrlGenerator.php
@@ -1,8 +1,22 @@
 <?php
+declare(strict_types=1);
 
 namespace App\Service\UrlGenerator;
 
-class ResetUrlGenerator
-{
+use League\Uri\UriTemplate;
 
+readonly class ResetUrlGenerator {
+    function __construct(private ClientUrlGenerator $urlGenerator) {}
+
+    /**
+     * Generate a UriTemplate based on the context of the request.
+     *
+     * `$this->generator->generate()->expand(['userId' => $userId, 'token' => $token]);`
+     *
+     * @return UriTemplate
+     * @see https://uri.thephpleague.com/uri/7.0/uri-template/
+     */
+    public function generate(): UriTemplate {
+        return new UriTemplate("{$this->urlGenerator->getClientUrl()}#/forgot/set?userId={userId}&token={token}");
+    }
 }
