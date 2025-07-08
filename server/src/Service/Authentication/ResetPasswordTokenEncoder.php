0ff9439e44e6316cee956a5b94a95c2e18328c7f
DankYoda <167357217+DankYoda@users.noreply.github.com>
2025-07-08 09:49:30 -0500

    Update ResetPasswordTokenEncoder.php

--- server/src/Service/Authentication/ResetPasswordTokenEncoder.php
+++ server/src/Service/Authentication/ResetPasswordTokenEncoder.php
@@ -2,7 +2,75 @@
 
 namespace App\Service\Authentication;
 
-class ResetPasswordTokenEncoder
-{
+use App\Exception\Resource\TokenExpired;
+use App\Exception\Resource\TokenInvalid;
+use App\Model\Entity\User;
+use App\Repository\UserRepository;
+use DateInterval;
+use ParagonIE\Paseto\Builder;
+use ParagonIE\Paseto\Exception\PasetoException;
+use ParagonIE\Paseto\Keys\SymmetricKey;
+use ParagonIE\Paseto\Protocol\Version3;
+use ParagonIE\Paseto\Purpose;
+use Psr\Log\LoggerInterface;
+use Symfony\Component\Clock\ClockInterface;
+use UnexpectedValueException;
 
+readonly class ResetPasswordTokenEncoder
+{
+	function __construct(
+		private string $password,
+		private UserRepository $userRepository,
+        private LoggerInterface $logger,
+        private TokenParser $tokenParser,
+        private ClockInterface $clock
+	)
+	{
+        if (strlen($this->password)<32)
+            throw new UnexpectedValueException("Token secret must be at least 32 characters.");
+	}
+
+    public function verify(string $accessToken): User {
+        try {
+            /** @noinspection DuplicatedCode */
+            $parsedToken = $this->tokenParser->parse($accessToken);
+
+            $user = $parsedToken->get('user') ? $this->userRepository->find($parsedToken->get('user')) : null;
+
+            // If the user does not exist
+            if (!$user) {
+                $this->logger->info('An unexpected error occurred: A token was decoded with an user that does not exist.');
+                throw new TokenInvalid();
+            }
+
+            // If the token issued date is before the user's from date
+            if ($user->getNotBefore() && ($parsedToken->getIssuedAt() < $user->getNotBefore())) {
+                throw new TokenExpired();
+            }
+
+            if ($parsedToken->get('type') !== 'password')
+                throw new TokenInvalid();
+        } catch (PasetoException) {
+            throw new TokenInvalid();
+        }
+
+        return $user;
+	}
+
+    public function create(User $user): string
+    {
+        $privateKey = new SymmetricKey(hash('sha256', $this->password, true), new Version3());
+        return (new Builder())
+            ->setKey($privateKey)
+            ->setVersion(new Version3())
+            ->setPurpose(Purpose::local())
+            ->setClaims([
+                'type'=>'password',
+                'user'=>$user->getId()
+            ])
+            ->setNotBefore($this->clock->now())
+            ->setExpiration($this->clock->now()->add(new DateInterval('PT10M')))
+            ->setIssuedAt($this->clock->now())
+            ->toString();
+    }
 }
