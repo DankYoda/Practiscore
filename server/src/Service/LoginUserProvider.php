3f82fe5ef6522c8e699c7b94eaaa652564567a66
dankyoda <dakotafeist@pm.com>
2025-08-18 18:46:22 -0500

    Created a login provider to retrieve by username instead od id

---
+++ server/src/Service/LoginUserProvider.php
@@ -0,0 +1,47 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Service;
+
+use App\Model\Entity\User;
+use App\Repository\UserRepository;
+use Symfony\Component\Security\Core\Exception\UnsupportedUserException;
+use Symfony\Component\Security\Core\Exception\UserNotFoundException;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Security\Core\User\UserProviderInterface;
+
+class LoginUserProvider implements UserProviderInterface
+{
+	function __construct(
+		private readonly UserRepository $userRepository,
+	)
+	{
+	
+	}
+	
+	public function refreshUser(UserInterface $user): UserInterface
+	{
+		if (!$user instanceof User) {
+			throw new UnsupportedUserException(sprintf('Invalid user class "%s".', $user::class));
+		}
+		
+		// Return a User object after making sure its data is "fresh".
+		// Or throw a UserNotFoundException if the user no longer exists.
+		throw new \Exception('TODO: fill in refreshUser() inside '.__FILE__);
+	}
+	
+	public function supportsClass(string $class): bool
+	{
+		return User::class === $class || is_subclass_of($class, User::class);
+	}
+	
+	public function loadUserByIdentifier(string $identifier): UserInterface
+	{
+		$user = $this->userRepository->findOneBy(['username' => $identifier]);
+		if (!$user)
+		{
+			throw new UserNotFoundException();
+		}
+		return $user;
+	}
+}
