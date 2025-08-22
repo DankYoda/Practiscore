65d575c1df592f8b6e74ce42d649370be0f0a9ab
dankyoda <dakotafeist@pm.me>
2025-08-22 14:08:15 -0500

    Initialized the UI

--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -1,13 +1,49 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import {Button} from "primeng/button";
+import {Menubar} from "primeng/menubar";
+import {MenuItem} from "primeng/api";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Button],
+    imports: [RouterOutlet, Menubar],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
-  protected title = 'client';
+export class App implements OnInit {
+    protected title = 'client';
+    items: MenuItem[] | undefined;
+    ngOnInit() {
+        this.items =
+        [
+            {
+                label: 'Home',
+                icon: 'pi pi-fw pi-file',
+                routerLink: '/home',
+            },
+            {
+                label: 'Scores',
+                icon: 'pi pi-fw pi-pencil',
+                routerLink: '/scores'
+            },
+            {
+                label: 'Events',
+                icon: 'pi pi-fw pi-question',
+                routerLink: '/events',
+                command: () => {
+                  // Execute a function when this item is clicked
+                  console.log('Help clicked!');
+                }
+            },
+            {
+                label: 'Matches',
+                icon: 'pi pi-fw pi-info',
+                routerLink: ['/matches']
+            },
+            {
+                label: 'Support',
+                icon: 'pi pi-fw pi-info',
+                routerLink: ['/support']
+            }
+        ];
+    }
 }
